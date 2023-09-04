8c95bbf351e7c44b51e8032deba692da3df3161a
Amairani Carrillo <cramairani5@gmail.com>
2023-09-03 18:53:31 -0600

    seleccionar todos los elementos que tenga etiqueta especifica html. Acceder a todos los elementos li de mi elemento ul

--- toppings-de-pizza/app.js
+++ toppings-de-pizza/app.js
@@ -1,5 +1,13 @@
 // const titulo = document.getElementById('titulo');
 // console.log(titulo);
 
-const toppings = document.getElementsByClassName('topping');
-console.log(toppings[0].id);
+// const toppings = document.getElementsByClassName('topping');
+// console.log(toppings[3]);
+
+//seleccionar todos los elementos que tenga etiqueta especifica html. Acceder a todos los elementos li de mi elemento ul 
+
+const misToppings = document.getElementsByTagName('li');
+console.log(misToppings);
+
+const toppingsFondoMarron = document.getElementsByClassNam('fondo-marron');
+console.log(toppingsFondoMarron);
