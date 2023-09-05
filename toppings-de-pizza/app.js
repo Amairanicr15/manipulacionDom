d6998da969e62fba107a866ad6544088daae4827
Amairani Carrillo <cramairani5@gmail.com>
2023-09-04 19:22:18 -0600

    seleccionar un elemento que cumple un criterio de un selector css. y es para hacerlo de una manera más específica

--- toppings-de-pizza/app.js
+++ toppings-de-pizza/app.js
@@ -6,8 +6,24 @@
 
 //seleccionar todos los elementos que tenga etiqueta especifica html. Acceder a todos los elementos li de mi elemento ul 
 
-const misToppings = document.getElementsByTagName('li');
-console.log(misToppings);
+// const misToppings = document.getElementsByTagName('li');
+// console.log(misToppings);
 
-const toppingsFondoMarron = document.getElementsByClassNam('fondo-marron');
-console.log(toppingsFondoMarron);
+// const toppingsFondoMarron = document.getElementsByClassNam('fondo-marron');
+// console.log(toppingsFondoMarron);
+
+//Seleccionar elementos en base a elementos que cumplen un selector css especifico   
+//.querySelector() nos permite seleccionar el primer criterio que cumpla 
+//.querySelectorAll() nos permite seleccionar todos los elementos que cumplan con ese criterio  
+
+// const primerToppinNaranja = document.querySelector('.topping.fondo-naranja');
+// console.log(typeof primerToppinNaranja);
+
+
+// usando la etiqueta ul
+// const primerToppinNaranja = document.querySelector('.ul li.fondo-naranja');
+// console.log(primerToppinNaranja);
+
+//seleccionar el primer topping que no tenga el fondo marraon
+const primerToppingNoMarron = document.querySelector('ul li:not(.fondo-marron)');
+console.log(primerToppingNoMarron);
