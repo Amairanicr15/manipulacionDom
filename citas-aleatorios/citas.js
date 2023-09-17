04bc7e9e54cce2a7d9d177c347c9c0e5e884eec3
Amairani Carrillo <cramairani5@gmail.com>
2023-09-16 20:30:55 -0600

    Se agregaron las citas en el archivo citas.js

--- citas-aleatorios/citas.js
+++ citas-aleatorios/citas.js
@@ -1,1 +1,26 @@
-let citas =
+let citas = [
+  {
+    'autor': 'Albert Einstein',
+    'texto': 'No poseo talentos especiales. Solo soy apasionadamente cursioso.'
+  },
+  {
+    'autor': 'Anónimo',
+    'texto': 'Semanas de programación te pueden ahorrar horas de planeación.'
+  },
+  {
+    'autor': 'Alan Kay',
+    'texto': 'La mejor forma de predecir el futuro es intervalo.'
+  },
+  {
+    'autor': 'Amelia Earhart',
+    'texto': 'Lo más díficil es tomar la decisión de actuar. El resto es...'
+  },
+  {
+    'autor': 'Jane Goodall',
+    'texto': 'Lo que haces hace un diferencia. Y debes decidir...'
+  },
+  {
+    'autor': 'Mark Twain',
+    'texto': 'Los dos días más importantes de tu vida son el...'
+  }
+];
